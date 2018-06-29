f65dc1c221ab0b69abc9969776b7876abe232677
айкеновад <@234_11>
2018-06-29 12:17:14 +0600

    -

--- 29.06.2018/Source.cpp
+++ 29.06.2018/Source.cpp
@@ -26,95 +26,157 @@ start:
 	{
 	case 1:
 	{
-		int n = 9;
-		for (int i = 0; i < n; i++)
+		int n;
+		int k;
+		cin >> n >> k;
+		int d = n;
+
+		if (n / k != 0) d = n - n%k;
+
+		for (int i = 0; i < (n / k); i++)
 		{
-			for (int j = 0; j < n; j++)
+			
+			for (int j = i + 1 ; j <= d; j=j+n/k)
 			{
-				if (i >= j && (i + j >= n - 1)) cout << "*";
-				else cout << " ";
+				cout << j <<" ";
 			}
 			cout << endl;
 		}
-		int m = 9;
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < m; j++)
+			for (int s = 1; s <= n%k; s++)
 			{
-				if (i <= j && (i + j <= m - 1)) cout << "*";
-				else cout << " ";
+				cout << d + s << " ";
 			}
 			cout << endl;
-		}
 	}
 	break;
 	case 2:
 	{
-		int height = 0;
-
-		cout << "Введите высоту  равнобедренного треугольника: ";
-		cin >> height;
-
-		for (int i = 0; i < height; i++)
+		int n;
+		cin >> n;
+		bool flag = false;
+		for (int i = 2; i < n; i++)
 		{
-			for (int j = 1; j < height - i; j++)
+			if (n%i == 0)
 			{
-				cout << ' ';
+				flag = true;
+				break;
 			}
-
-			for (int j = height - 2 * i; j <= height; j++)
-			{
-				cout << '*';
-			}
-			cout << endl;
 		}
+		if (flag)
+			cout << "number is not prime" << endl;
+		else cout << "Number is prime" << endl;
+
 	}
 	break;
 	case 3:
 	{
-		int height = 0;
+		int n;
+		bool flag = false;
 
-		cout << "Введите высоту  равнобедренного треугольника: ";
-		cin >> height;
-
-		for (int i = 0; i < height; i++)
+		while(true)
 		{
-			for (int j = 1; j < height - i; j++)
+			flag = false;
+			cin >> n;
+
+			for (int i = 2; i < n; i++)
 			{
-				cout << ' ';
+				if (n%i == 0)
+				{
+					flag = true;
+					break;
+				}
 			}
 
-			for (int j = height - 2 * i; j <= height; j++)
+			if (flag)
+				cout << "number is not prime" << endl;
+			else
 			{
-				cout << '*';
+				cout << "Number is prime" << endl;
+
 			}
-			cout << endl;
 		}
 	}
 	break;
-	case 4:
+	case 4: //85
 	{
+		double a;
+		double h;
+		int n;
+		cin >> a >> h >> n;
+		int sum = 0;
 
+		for (int i = 0; i <= n; i++)
+		{
+			if(i==0||i==n)
+			sum = sum + (pow((a + h*i), 2) + 1)*pow(cos(a + h*i), 2);
+			else sum = sum + 2*(pow((a + h*i), 2) + 1)*pow(cos(a + h*i), 2);
+		}
+		cout << sum << endl;
 	}
 	break;
 	case 5:
 	{
+		int n;
+		cin >> n;
+		int x = 0;
 
+		while (n/2>0)
+		{
+			cout << n % 2;
+			n = n / 2;
+			if (n / 2==0) cout << 1;
+		}
+		cout << endl;
 	}
 	break;
-	case 6:
+	case 6: //110
 	{
+		int n = 0;
+		double sum = 0;
 
+		for (int i = 103; i > 0; i -= 2)
+		{
+			sum = 1/(i + sum);
+		}
+		cout << sum << endl << endl;
 	}
 	break;
-	case 7:
+	case 7: //117 a
 	{
+		cout << "117 a" << endl;
+		cout << "enter n: ";
+		int n;
+		cin >> n;
+		double p = 1;
+		int i = 1;
 
+		while (n > i/2)
+		{
+			p = p*(i / (i + 1.0));
+			i += 2;
+		}
+		cout << p << endl << endl;
 	}
 	break;
 	case 8:
 	{
+		//117 б
+		cout << "117 б" << endl;
 
+		int n;
+		cout << "enter n: ";
+		cin >> n;
+		double p = 1;
+		double i = 1;
+		double j = 1;
+
+		while (n >= j)
+		{
+			p = p*(i / j);
+			i += 2;
+			j++;
+		}
+		cout << p << endl << endl;
 	}
 	break;
 	case 9:
